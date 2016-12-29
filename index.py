b49e98c824927d768579db844928efb3787cd372
Jay Shenoy <jayshenoy@hotmail.com>
2016-12-29 15:04:15 -0500

    Test with multiple songs

--- index.py
+++ index.py
@@ -1,25 +1,26 @@
 import midi
 from PIL import Image
-from math import floor, sqrt
+from math import floor, sqrt, sin
+import os
 
-pattern = midi.read_midifile("test.mid")
+for filename in os.listdir('music'):
+    if filename.endswith('.mid'):
+        pattern = midi.read_midifile('music/%s' % filename)
 
-notes = []
+        notes = []
 
-for a in pattern:
-    for b in a:
-        if "Note" in str(b):
-            notes.append(b)
+        for a in pattern:
+            for b in a:
+                if "Note" in str(b):
+                    notes.append(b)
 
-print notes
+        size = int(floor(sqrt(len(notes))))
 
-size = int(floor(sqrt(len(notes))))
+        im = Image.new("RGB", (size, size), "white")
 
-im = Image.new("RGB", (size, size), "white")
+        for i in range(size):
+            for j in range(size):
+                note = notes[i * size + j]
+                im.putpixel((i, j), (int(abs(sin(note.tick)) * 255), int(abs(sin(note.data[0])) * 255), int(abs(sin(note.data[1])) * 255)))
 
-for i in range(size):
-    for j in range(size):
-        note = notes[i * size + j]
-        im.putpixel((i, j), (note.tick % 256, note.data[0] % 256, note.data[1] % 256))
-
-im.show()
+        im.save('images/%s' % filename.replace('.mid', '.png'), 'PNG')
