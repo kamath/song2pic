91496377923f5f1050ddd315702d1fdbc7820089
Jay Shenoy <jayshenoy@hotmail.com>
2016-12-29 14:32:00 -0500

    Convert music to image

--- #index.py#
+++
@@ -1,3 +0,0 @@
-import midi
-pattern = midi.read_midifile("test.mid")
-print pattern
--- index.py
+++ index.py
@@ -1,11 +1,25 @@
-
 import midi
+from PIL import Image
+from math import floor, sqrt
+
 pattern = midi.read_midifile("test.mid")
 
 notes = []
 
 for a in pattern:
     for b in a:
-        if "Note" in str(b) and a.tick and a.data:
+        if "Note" in str(b):
             notes.append(b)
 
+print notes
+
+size = int(floor(sqrt(len(notes))))
+
+im = Image.new("RGB", (size, size), "white")
+
+for i in range(size):
+    for j in range(size):
+        note = notes[i * size + j]
+        im.putpixel((i, j), (note.tick % 256, note.data[0] % 256, note.data[1] % 256))
+
+im.show()
