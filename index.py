91b526e9c9463ff3fe232ecb37d5c5ceb1d10c78
Andy <andy@andykamath.com>
2018-03-24 12:59:21 -0400

    update!

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import midi
 from PIL import Image
 from math import floor, sqrt, sin
 import os
+import cv2
 
 for filename in os.listdir('music'):
     if filename.endswith('.mid'):
@@ -22,5 +23,9 @@ for filename in os.listdir('music'):
             for j in range(size):
                 note = notes[i * size + j]
                 im.putpixel((i, j), (int(abs(sin(note.tick)) * 255), int(abs(sin(note.data[0])) * 255), int(abs(sin(note.data[1])) * 255)))
-
+        im.save('images/%s' % filename.replace('.mid', '.png'), 'PNG')
+        original_image = cv2.imread('images/%s' % filename.replace('.mid', '.png'),0)
+        original_height, original_width = original_image.shape[:2]
+        factor = 5
+        im = im.resize((10000, 10000))
         im.save('images/%s' % filename.replace('.mid', '.png'), 'PNG')
